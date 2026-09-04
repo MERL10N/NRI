bf76eb0a190bd67b921bb7838fd3429fea46cf96
Kian <92978255+MERL10N@users.noreply.github.com>
2026-09-04 00:11:10 -0700

    Add member functions and member variables in DeviceMTL.h

--- Source/MTL/DeviceMTL.h
+++ Source/MTL/DeviceMTL.h
@@ -10,8 +10,19 @@ namespace nri {
          return m_Device;
      }
 
+     inline MTL::Device* GetResidencySet() const {
+         return m_ResidencySet;
+     }
+
+     void AddToResidencySet(MTL::Allocation* allocation);
+     void RemoveFromResidencySet(MTL::Allocation* allocation);
+     void CommitResidencySet();
+
+     Result Create(const CallbackInterface& callbacks);
+
  private:
      MTL::Device* m_Device = nullptr;
+     MTL::ResidencySet m_ResidencySet = nullptr;
  };
 }
 #endif // NRI_DEVICEMTL_H
